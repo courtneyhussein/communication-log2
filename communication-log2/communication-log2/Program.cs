638b6d58b2d07f2d5597713966b03a04557f898a
CourtneyHussein <courtneyhussein124@gmail.com>
2021-06-22 15:33:11 -0400

    Added wheels down comment

--- communication-log2/communication-log2/Program.cs
+++ communication-log2/communication-log2/Program.cs
@@ -6,11 +6,12 @@ namespace communication_log2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Almost the weekend!");
             Console.WriteLine("Hello again!");
             Console.WriteLine("Cleared to fly!");
             Console.WriteLine("We forgot the navigator!");
             Console.WriteLine("Cleared to land");
+            Console.WriteLine("wheels down.");
         }
     }
 }
